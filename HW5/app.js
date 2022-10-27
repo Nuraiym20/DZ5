d49a4d7eab11b7f94645985b28f24b1ea578e2a2
Nuraiym20 <114159583+Nuraiym20@users.noreply.github.com>
2022-10-27 21:22:21 +0600

    Add files via upload

---
+++ HW5/app.js
@@ -0,0 +1,32 @@
+const som = document.querySelector("#som");
+const usd = document.querySelector("#usd");
+const euro = document.querySelector("#euro")
+
+
+const handleConvert = (elem, target, target2, isTrue , isTrue2) => {
+    elem.addEventListener("input", () => {
+        const request = new XMLHttpRequest();
+        request.open("GET", "data.json");
+        request.setRequestHeader("Content-type", "application/json");
+        request.send();
+        request.addEventListener("load", () => {
+            const response = JSON.parse(request.response);
+
+            if(isTrue) {
+                    target.value = (+elem.value / response.euro).toFixed(2)
+                    target2.value= (+elem.value / response.usd).toFixed(2);
+            } else if (isTrue2) {
+                    target.value = (+elem.value / response.euro).toFixed(2)
+                    target2.value = (+elem.value * response.usd / response.euro).toFixed(2);
+            } else {
+                    target.value = (+elem.value / response.usd).toFixed(2)
+                    target2.value = (+elem.value * response.euro / response.usd).toFixed(2);
+            }
+            elem.value === "" ? (target.value = "") : null;  
+            elem.value === "" ? (target2.value = "") : null ;
+        });
+    });
+};
+handleConvert(som,euro,usd,true);
+handleConvert(euro, som, usd,false,true);
+handleConvert(usd, som, euro)
